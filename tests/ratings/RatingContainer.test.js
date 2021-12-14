81d4b2fdba78cecf37188659fe55908bd93ff2fa
Joseph Nahm <josephnahm1@gmail.com>
2021-12-13 21:06:44 -0800

    Webpack config updated, stylesheet for index added, Basic Tests Implemented, Rendering of Reviews Module
    -Webpack test for css fixed to be another rule set
    -Added index.css for a basic global stylesheet, mainly to render Ratings container in middle of screen and added a border for easy visualization
    -Tests fixed to pass with all components

--- tests/ratings/RatingContainer.test.js
+++ tests/ratings/RatingContainer.test.js
@@ -4,6 +4,7 @@ import RatingContainer from '../../client/src/Ratings/RatingContainer';
 
 test('Rating Container renders', () => {
   var { getByTestId } = render(<RatingContainer/>);
-  var divEl = getByTestId('practice-div');
-  expect(divEl.textContent).toBe('This is a Practice component');
+  var containerEl = getByTestId('container');
+
+  expect(containerEl.textContent).toBe('ReviewModuleThis is a Practice componentThis is a Practice componentThis is a Practice componentThis is a Practice componentThis is a Practice component');
 })
