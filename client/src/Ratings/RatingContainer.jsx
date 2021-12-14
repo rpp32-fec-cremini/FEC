a1de270e93b707b939b082abf5b7f13d19e84c4d
Joseph Nahm <josephnahm1@gmail.com>
2021-12-13 20:35:13 -0800

    Commiting client side files as well

---
+++ client/src/Ratings/RatingContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ReviewsList from "./ReviewsList";
+import AddReview from "./AddReview";
+import SortReviews from "./SortReviews";
+import RatingBreakdown from "./RatingBreakdown";
+import ProductBreakdown from "./ProductBreakdown";
+
+class ReviewModule extends React.Component {
+  render() {
+    return <div data-testid="practice-div">This is a Practice component</div>
+  }
+}
+
+export default ReviewModule;
