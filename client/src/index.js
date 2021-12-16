4d3ffe95b1a6e34815e07d1c9ad5fc0645e642e8
yytt77 <yytt77110@gmail.com>
2021-12-16 00:01:18 -0500

    New submit

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RatingContainer from '../src/Ratings/RatingContainer.jsx';
-import QandA from '../src/Q&A/QandA.jsx';
+import RelatedList from '../src/Related/RelatedList.jsx';
 import "./index.css"
+import QandA from '../src/Q&A/QandA.jsx';
 
 var App = (props) => (
   <div id="app">
