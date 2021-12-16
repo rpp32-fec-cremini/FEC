fef589af155b55a1af1bc21a78bc276893195019
bonnie <arlolocos@gmail.com>
2021-12-15 19:58:58 -0500

    Related list retrieves sample data and displays correctly

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RatingContainer from '../src/Ratings/RatingContainer.jsx';
+import RelatedList from '../src/Related/RelatedList.jsx';
 import "./index.css"
 
 var App = (props) => (
-  <RatingContainer/>
+  <div>
+    <RelatedList />
+    <RatingContainer />
+  </div>
 )
 
-ReactDOM.render(<App className="App"/>, document.getElementById('root'));
+ReactDOM.render(<App className="App" />, document.getElementById('root'));
--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ app.use(express.json())
 var reviewsRouter = require("./routes/Reviews");
 app.use("/reviews", reviewsRouter);
 
+var relatedRouter = require('./routes/Related');
+app.use('/products', relatedRouter);
+
 app.listen(port, () => {
   console.log('App listening on port: ', port);
 })
