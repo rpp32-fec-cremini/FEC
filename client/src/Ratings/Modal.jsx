2aec88e8ec24e4974459fc1db3a1f2fe1b94d832
Joseph Nahm <josephnahm1@gmail.com>
2021-12-17 18:29:40 -0800

    implemented and tested modal window

--- client/src/Ratings/Modal.jsx
+++ client/src/Ratings/Modal.jsx
@@ -11,9 +11,9 @@ var Modal = ({images}) => {
         setModal(e.target.src)
         setDisplay("block")
       }}></img>)}
-      <div id="myModal" className="modal" style={{display}}>
+      <div id="myModal" className="modal" style={{display}} data-testid="modal">
         <div className="modal-content">
-          <span className="close" onClick={() => setDisplay("none")}>&times;</span>
+          <span className="close" onClick={() => setDisplay("none")} data-testid="close">&times;</span>
           <div style={{
             'height': '80%',
             'width': '100%',
