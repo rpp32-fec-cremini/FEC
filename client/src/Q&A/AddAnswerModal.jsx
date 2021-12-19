70af9fab9deb84f467cdc7adb0a94a312246de77
yytt77 <yytt77110@gmail.com>
2021-12-18 21:57:23 -0500

    Completed Sorted Answer

--- client/src/Q&A/AddAnswerModal.jsx
+++ client/src/Q&A/AddAnswerModal.jsx
@@ -4,27 +4,48 @@ const AddAnswerModal = ({answer}) => {
   const [loading, setloading] = useState(false);
   const [size, setsize] = useState(0);
 
+
   const showbutton = () => {
     if (Object.keys(answer).length > 2) {
       return <button onClick={() => getMoreAnswer(answer)}>See more answers</button>
     }
   }
-  const show = (answer, moreThanTwo) => {
-    if (moreThanTwo === true) {
-      var twoAnswer = Object.keys(answer).slice(0, 2)
-    } else {
-      var twoAnswer = Object.keys(answer)
+
+  const collaspseButton = () => {
+    return <button onClick={() => collapsedAnswer()}>Collasped answer</button>
+  }
+
+  const collapsedAnswer = () => {
+    setloading(false);
+    setsize(0);
+  }
+
+  const show = (answerarr, moreThanTwo) => {
+    var answer = [];
+    for (var item in answerarr) {
+      answer.push([item, answerarr[item]]);
     }
-    return twoAnswer.map(key =>
-      <div key={answer[key]['id']}> A: {answer[key]['body']}
-        <div>by {answer[key]['answerer_name']},{dateConvenver(answer[key]['date'])}  |  Helpful? YES({answer[key]['helpfulness']})</div>
+
+    answer.sort(function(a, b) {
+      return (a[1]['helpfulness']) - (b[1]['helpfulness'])
+    });
+
+    if (moreThanTwo === true) {
+      var twoAnswer = answer.slice(0, 2)
+    } else {
+      var twoAnswer = answer
+    }
+    console.log('this is the answer', twoAnswer);
+    return twoAnswer.map(answer =>
+      <div key={answer[1]['id']}> A: {answer[1]['body']}
+        <div>by {answer[1]['answerer_name']},{dateConvenver(answer[1]['date'])}  |  Helpful? YES({answer[1]['helpfulness']})</div>
       </div>
     )
   }
 
   const getMoreAnswer = (answer) => {
     setloading(true);
-    setsize('length', Object.keys(answer).length)
+    setsize(Object.keys(answer).length)
   }
 
   const dateConvenver = (date) => {
@@ -38,7 +59,7 @@ const AddAnswerModal = ({answer}) => {
         <div>
           {loading === false && size < 2 ? show(answer, true) : show(answer)}
         </div>
-        {loading === false ? showbutton() : null}
+        {loading === false ? showbutton() : collaspseButton()}
     </div>
   )
 
--- client/src/Q&A/IndividualQuestion.jsx
+++ client/src/Q&A/IndividualQuestion.jsx
@@ -1,13 +1,9 @@
 import React, {useState} from 'react';
 import AddAnswerModal from './AddAnswerModal.jsx';
 
-
-
-
 const IndividualQuestion = (props) => {
-
   return (
-    <div className = "Questions">
+    <div className = "Questions" style={{overflowY: 'scroll', height:'500px'}}>
       {props.question.map(question =>
         <div className="Question-row" key = {question.question_id} >
           <div className="Question-body" style={{float: "left"}}>Q: {question.question_body}</div>
