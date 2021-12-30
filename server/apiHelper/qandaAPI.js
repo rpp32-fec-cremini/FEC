8680a90c87dbc4f23dd71310764f4cf548b459f7
yytt77 <yytt77110@gmail.com>
2021-12-29 22:04:31 -0500

    Start to work on post request

--- server/apiHelper/qandaAPI.js
+++ server/apiHelper/qandaAPI.js
@@ -1,16 +1,23 @@
 const axios = require('axios');
-// const config = require('../../.env/config');
-// require('dotenv').config();
-var token = process.env.TOKEN;
 
-/* let getProducts = () => {
+let getProducts = (endPoint) => {
     let options = {
-        url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions?product_id=59553',
+        url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions${endPoint}`,
+        // url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions/553673/answers',
+        // url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products`,
         headers: {
-            'Authorization': token
+            'Authorization': process.env.TOKEN
+
         }
     }
     return axios(options);
-}; */
+};
 
-/* module.exports.getProducts = getProducts; */
+// getProducts('?product_id=59553')
+// .then(question=> {
+//     console.log('question', question.data)
+//   console.log('data', question.data.results[0].answers);
+// //   res.send(JSON.stringify(question.data.results));
+// })
+
+module.exports = getProducts;
