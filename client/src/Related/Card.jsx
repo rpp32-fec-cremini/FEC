67b699d76e61f2ddcb2f138a06902fcc9c996c6d
bonnie <arlolocos@gmail.com>
2022-01-05 16:31:09 -0500

    pulling prices from api, implemeted price component

--- client/src/Related/Card.jsx
+++ client/src/Related/Card.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import $ from 'jquery';
 import './related.css';
+import Price from './Price.jsx'
 import { IoAdd } from "react-icons/io5";
 import { IoIosStarHalf } from "react-icons/io";
 import { IoIosStarOutline } from "react-icons/io";
@@ -13,14 +15,16 @@ class Card extends React.Component {
     this.state = {
       actionName: TiDeleteOutline,
       currentImage: null,
-      currentPrice: null
+      salePrice: null,
+      regPrice: null,
+      sale: false
     }
   };
 
   componentDidMount() {
     // this.displayWidth();
     this.getType();
-    this.getStyleInfo(this.props.product.id, this.props.product.name);
+    this.getStyleInfo(this.props.product.id, this.props.product.name, this.props.product.default_price);
   }
 
   // displayWidth = () => {
@@ -47,15 +51,28 @@ class Card extends React.Component {
   //     });
   // }
 
-  getStyleInfo = async (id, name) => {
+  getStyleInfo = async (id, name, price) => {
     try {
-      let styles = await axios.get(`/products/${id}/styles`);
-      if (styles.data.results[0].photos[0].thumbnail_url) {
-        this.setState({ currentImage: styles.data.results[0].photos[0].thumbnail_url })
+      let discount = null;
+      let results = await axios.get(`/products/${id}/styles`);
+      let styles = results.data.results;
+      let defaultStyle = styles.find(style => style['default?'] === true) || styles[0];
+      let defaultIndex = styles.findIndex(style => style['default?'] === true);
+      defaultIndex = defaultIndex < 0 ? 0 : defaultIndex;
+      // console.log(name, defaultStyle.photos[0].thumbnail_url, defaultIndex);
+      // console.log(name, defaultStyle.sale_price, defaultIndex);
+      if (defaultStyle.photos[0].thumbnail_url) {
+        this.setState({ currentImage: defaultStyle.photos[0].thumbnail_url })
       } else {
         let productLabel = name.toLowerCase().split(' ');
         this.setState({ currentImage: `https://source.unsplash.com/230x330/?${productLabel[productLabel.length - 1]}` });
       }
+      console.log(name, price, defaultIndex);
+      // defaultStyle.sale_price ?
+      //   this.setState({salePrice: (price - defaultStyle.sale_price).toFixed(2), regPrice: price, sale: true }) : this.setState({ regPrice: price });
+      discount ?
+        this.setState({ salePrice: (price - discount).toFixed(2), regPrice: price, sale: true }) : this.setState({ regPrice: price });
+      console.log(this.state.salePrice, this.state.regPrice);
     } catch (err) {
       console.log(err);
     }
@@ -67,6 +84,10 @@ class Card extends React.Component {
 
   render() {
     let Action = this.state.actionName;
+    let finalPrice = this.state.price;
+    if (this.state.sale) {
+
+    }
     if (this.props.product.id === '000') {
       return (
         <li data-testid='card' className='related-card related-add' onClick={(product) => this.props.addClick(this.props.product)}>
@@ -86,7 +107,8 @@ class Card extends React.Component {
           <div className='card-text'>
             <p className='card-category'>{this.props.product.category.toUpperCase()} </p>
             <p className='card-name'>{this.props.product.name}</p>
-            <p className='card-price'> $100</p>
+            <Price salePrice={this.state.salePrice} regPrice={this.state.regPrice} sale={this.state.sale} />
+            {/* <p className='card-price'> {`$${this.state.currentPrice}`}</p> */}
             <div className='card-stars'>
               <IoIosStarOutline className='card-star' />
               <IoIosStarOutline className='card-star' />
