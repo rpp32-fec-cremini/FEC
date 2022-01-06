12ee057ef092d38a2655090f324a2e60c61518cf
yytt77 <yytt77110@gmail.com>
2022-01-05 21:11:51 -0500

    Start to work on image uplpad

--- server/apiHelper/qandaAPI.js
+++ server/apiHelper/qandaAPI.js
@@ -29,9 +29,27 @@ const postQuestion = (data) => {
     })
 };
 
+const postAnswer = (data, questionId) => {
+    // var product_id = parseInt(data.product_id);
+    var questionId = questionId;
+    const params = {
+        body: data.question,
+        name: data.nickName,
+        email: data.email,
+        photos: []
+    };
+    console.log('this is the data', params, questionId);
+    // params = JSON.stringify(params)
+    return axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions${questionId}`, params, {
+        headers: {
+            'Authorization': process.env.TOKEN
+        }
+    })
+};
+
 // getProducts('?product_id=59555')
 // .then(question=> {
-//     console.log('question', question.data)
+//     // console.log('question', question.data)
 //   console.log('data', question.data.results[0].answers);
 // //   res.send(JSON.stringify(question.data.results));
 // })
@@ -48,5 +66,6 @@ const postQuestion = (data) => {
 module.exports = {
     getProducts,
     postQuestion,
+    postAnswer,
 }
 // module.exports = postQuestion;
