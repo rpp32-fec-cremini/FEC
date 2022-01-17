a81cf02f10847344ec58e5fa7c374f45d83e2399
Joseph Nahm <josephnahm1@gmail.com>
2022-01-17 14:36:59 -0800

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'FEC',
+    script: 'server/server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-50-18-105-138.us-west-1.compute.amazonaws.com',
+      key: 'fec.pem',
+      ref: 'origin/main',
+      repo: 'https://github.com/rpp32-fec-cremini/FEC.git',
+      path: '/home/ubuntu/FECProd',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
