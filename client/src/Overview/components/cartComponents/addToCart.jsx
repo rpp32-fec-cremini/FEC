1047ff8373f570cc6c335410c0577e24e177d576
Austin Cobb <aunecobb@gmail.com>
2022-01-19 19:42:42 -0500

    Added hooks for cart

--- client/src/Overview/components/cartComponents/addToCart.jsx
+++ client/src/Overview/components/cartComponents/addToCart.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 //import CartItem from './cartComponents/cartItems.jsx';
@@ -11,8 +11,25 @@ const AddToCart = (props) =>  {
   var sizes = [];
   var quantities = [];
 
+  const [ size, setSize ] = useState('');
+  const [ quantity, setQuantity ] = useState('1');
+  const [ buttonDisable, setButton ] = useState(false);
+  const [ quantityDisable, setQDropDown ] = useState(true);
+
+  function sizeHandler(e) {
+    var value = document.getElementById('sizeSelect').value;
+    console.log('SIZE CLICKED!! ', value)
+    setSize(value);
+  }
+
+  function quantityHandler(e) {
+    setQuantity(e.target.value);
+  }
+
+
+
   for(var key in sku) {
-    console.log (`SIZE HERE: ${sku[key].size}, and quantity here: ${sku[key].quantity}`)
+    //console.log (`SIZE HERE: ${sku[key].size}, and quantity here: ${sku[key].quantity}`)
     sizes.push(sku[key].size);
     quantities.push(sku[key].quantity)
     totalQuantity += sku[key].quantity
@@ -20,56 +37,55 @@ const AddToCart = (props) =>  {
       console.log('SOLD OUT!!');
     }
   } console.log('HERES THE TOTAL ', totalQuantity)
+    console.log('Yo yo yo', size)
 
   const renderSelection = () => {
-
+    ///////Checking to see if any props have been passed in
     if(!props) {
       return
         <div>
           <h1>NO SKUUUUUSS, VERY SUS</h1>
         </div>
 
+    ///////If the total quantity of all size of the specific style is 0
     } else if (totalQuantity === 0) {
 
       return
         <div>
         <select name ="Select Size" defaultValue = 'Select Size' className = "selectors spaceRight">
           <option value = "Select Size" >Out of Stock!</option>
-          {/* <option value = "Select Size" selected disabled hidden>Select Size</option> */}
         </select>
-        {/* <select name = "Select Quantity" className = "selectors">
-        <option value = "Select Quantity" selected disabled hidden>Quantity</option>
-        </select> */}
         <br></br>
         <br></br>
-        <button>Add to Cart</button>
+        <button hidden>Add to Cart</button>
         </div>
 
+    ///// If everything comes out right and with quantity
     } else {
       return <div>
-      <select name ="Select Size" defaultValue = 'Select Size' className = "selectors spaceRight">
+      <select name ="Select Size" defaultValue = 'Select Size' className = "selectors spaceRight" id="sizeSelect" onChange = {sizeHandler}>
       <option value = "Select Size" hidden >Select Size</option>
        {sizes.map((sku) =>
-          <option value = {sku}>{sku}</option>
+          <option value = {sku} >{sku}</option>
           )}
       </select>
-      <select name = "Select Quantity" defaultValue = 'Select Quantity' className = "selectors">
-      <option value = "Select Quantity" disabled hidden>-</option>
-      {quantities.map((q) =>
-          <option value = {q}>{q}</option>
-          )}
+
+      <select name = "Select Quantity" defaultValue = 'Select Quantity' className = "selectors" disabled={quantityDisable}>
+      <option value = "Select Quantity" hidden>-</option>
+      {console.log('Current Quantity for SKU ', sku)}
       </select>
+
       <br></br>
       <br></br>
-      <button>Add to Cart</button>
+      <button disabled={buttonDisable} >Add to Cart</button>
       </div>
     }
   }
 
-    return (
-      <div>
-        {renderSelection()}
-      </div>
-    )}
+  return (
+    <div>
+      {renderSelection()}
+    </div>
+  )}
 
 export default AddToCart;
