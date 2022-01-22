ca065dc6559d4413030a4a5fbfe67960026b9748
yytt77 <yytt77110@gmail.com>
2022-01-21 21:55:45 -0500

    Updated Final CSS for QA

--- client/src/Q&A/SearchQuestions.jsx
+++ client/src/Q&A/SearchQuestions.jsx
@@ -22,7 +22,7 @@ const SearchQuestion = (props, {clicked}) => {
     <div onClick={(e) => props.clicked(e)}>
       <form className = "searchBox" onSubmit={onSubmit}>
         <label >
-          <input type = "text" data-testid = 'searchQuestion' value = {searchWord} placeholder = "Have a question? Search for answers..." onChange = {(e) => handleSearch(e, props) } style={{width: "80%", height:"30px",}}/>
+          <input className = "searchBar" type = "text" data-testid = 'searchQuestion' value = {searchWord} placeholder = "Have a question? Search for answers..." onChange = {(e) => handleSearch(e, props) } style={{width: "80%", height:"30px",}}/>
           <button className = "searchSubmitBtn" ><i className="fa fa-search"></i>Search</button>
         </label>
       </form>
