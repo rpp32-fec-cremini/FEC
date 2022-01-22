7d3a8f8bcb777f120acd3981dcaad538c948c138
yytt77 <yytt77110@gmail.com>
2022-01-21 20:36:33 -0500

    Complete QA

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import RatingContainer from '../src/Ratings/RatingContainer.jsx';
--- server/apiHelper/qandaAPI.js
+++ server/apiHelper/qandaAPI.js
@@ -3,8 +3,6 @@ const axios = require('axios');
 let getProducts = (endPoint) => {
     let options = {
         url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions${endPoint}`,
-        // url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions/553673/answers',
-        // url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products`,
         headers: {
             'Authorization': process.env.TOKEN
         }
@@ -20,8 +18,6 @@ const postQuestion = (data) => {
         email: data.email,
         product_id: product_id,
     };
-    console.log('this is the data', params);
-    // params = JSON.stringify(params)
     return axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions', params, {
         headers: {
             'Authorization': process.env.TOKEN
@@ -30,7 +26,6 @@ const postQuestion = (data) => {
 };
 
 const postAnswer = (data, questionId) => {
-    // var product_id = parseInt(data.product_id);
     var questionId = questionId;
     const params = {
         body: data.question,
@@ -38,8 +33,6 @@ const postAnswer = (data, questionId) => {
         email: data.email,
         photos: data.file
     };
-    console.log('this is the data', params, questionId);
-    // params = JSON.stringify(params)
     return axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions${questionId}`, params, {
         headers: {
             'Authorization': process.env.TOKEN
@@ -59,7 +52,6 @@ const questionHelpfulandReport = (endPoint) => {
 };
 
 const answerHelpfulandReport = (endPoint) => {
-    console.log('what is this endpoint', endPoint)
     let options = {
         method: 'PUT',
         url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/answers${endPoint}`,
@@ -69,19 +61,6 @@ const answerHelpfulandReport = (endPoint) => {
     }
     return axios(options);
 };
-
-// const answerHelpful = (endPoint) => {
-//     let options = {
-//         method: 'PUT',
-//         url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/answers${endPoint}`,
-//         headers: {
-//             'Authorization': process.env.TOKEN
-//         }
-//     }
-//     return axios(options);
-// };
-
-
 
 module.exports = {
     getProducts,
